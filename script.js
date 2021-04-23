3fd1092128d575715b490e8217a4eff4b0b983de
Melandin <82182575+Melandin@users.noreply.github.com>
2021-04-23 23:29:14 +0300

    update

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const variableText = document.querySelector('.variable-text');
 const value = document.getElementById('value');
 const grad = document.getElementById('text-grad');
 
+<<<<<<< Updated upstream
 grad.oninput = function () {
   console.log(this.value);
   variableText.style.fontVariationSettings = `'ital' ${this.value}`;
 };
+
+
