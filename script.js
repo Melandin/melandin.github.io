890b31d980b87e6ec287259ed5f23ea2a3d91c6f
Melandin <82182575+Melandin@users.noreply.github.com>
2021-04-24 01:26:34 +0300

    again

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ grad.oninput = function () {
   console.log(this.value);
   variableText.style.fontVariationSettings = `'ital' ${this.value}`;
 };
+
+
+const variableTextopsz = document.querySelector('.variable-textopsz');
+const valueopsz = document.getElementById('valueopsz');
+const gradopsz = document.getElementById('text-gradopsz');
+
+opsz.oninput = function () {
+  console.log(this.value);
+  variableText.style.fontVariationSettings = `'opsz' ${this.value}`;
+};
